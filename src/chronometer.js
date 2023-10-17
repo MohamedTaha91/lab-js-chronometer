d76eda0e4f1e60cdde355a448795fa8ff9563a17
Mohamed Taha <Taha_Acme@hotmail.com>
2023-10-17 16:24:34 +0200

    Lab finished

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+  this.intervalID = setInterval(()=>{
+  
+    console.log(this.currentTime)
+    if(printTimeCallback){
+      printTimeCallback()
+    }
+
+    this.currentTime++
+
+},1000 )
   }
 
   getMinutes() {
-    // ... your code goes here
+  return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+  return Math.floor(this.currentTime %60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value < 10 ? `0${value}` : `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalID)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    
   }
 }
